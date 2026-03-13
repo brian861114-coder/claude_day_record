740e7e5e60983b80c1c449f7855e3c2c630bf158
brian861114-coder <brian861114-coder@users.noreply.github.com>
2026-03-13 07:14:23 +0000

    deploy: 0274bbbd23d00f127c4db2fad7735f3153c66510

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:day_record/main.dart';
-
-void main() {
-  testWidgets('App starts successfully', (WidgetTester tester) async {
-    await tester.pumpWidget(const DayRecordApp());
-    expect(find.text('每日成長與\n開發實踐紀錄'), findsOneWidget);
-  });
-}
